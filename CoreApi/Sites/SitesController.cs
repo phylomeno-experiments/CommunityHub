786684e4921570f1b338d4d1124007ff1e530788
Roman Scheidegger <Roman.Scheidegger@zuehlke.com>
2018-06-28 21:40:55 +0200

    Create initial dummy sites controller

---
+++ CoreApi/Sites/SitesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Sites
+{
+    [Route("api/[controller]")]
+    public class SitesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(new[] { "Twitter", "YouTube" });
+        }
+    }
+}
