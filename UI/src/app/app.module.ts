25b241b969f50d9a9c55d5252f502d07319c5b3f
Roman Scheidegger <Roman.Scheidegger@zuehlke.com>
2018-06-29 17:43:15 +0200

    static iframe component routed

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IframeSiteComponent } from './iframe-site/iframe-site.component';
+
+const appRoutes: Routes = [
+  { path: 'iframe-site', component: IframeSiteComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IframeSiteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ UI/src/app/iframe-site/iframe-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iframe-site',
+  templateUrl: './iframe-site.component.html',
+  styleUrls: ['./iframe-site.component.css']
+})
+export class IframeSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
