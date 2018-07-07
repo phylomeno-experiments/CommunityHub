a38303747c20e7e86f0f2d036fec7dbb2bd5844c
Roman Scheidegger <roman.scheidegger@gmail.com>
2018-07-07 07:50:42 +0200

    introduce main ApiController

---
+++ CoreApi/Api/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Api
+{
+    [Route("api")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(void), 200)]
+        public IActionResult GetApi() 
+        {
+            return Ok();
+        }
+    }
+}
